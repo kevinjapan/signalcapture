53bab419ba5fd178c2f15d07d8f85a9fd97e0d80
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-11-13 11:17:43 +0000

    Update BrowseView.vue

--- src/views/BrowseView.vue
+++ src/views/BrowseView.vue
@@ -1,48 +1,52 @@
 <script setup lang="ts">
-import { onBeforeMount } from 'vue'
-// import { useCollectionItemsStore } from '../stores/collectionItemsStore'
-import useFetch from '../utilities/useFetch/useFetch'
-import CollectionItemTeaser from '../components/CollectionsItems/CollectionsItemTeaser/CollectionsItemTeaser.vue'
+import { ref, watchEffect } from 'vue'
+import { useCollectionsItemsListStore } from '../stores/collectionsItemsListStore'
+import CollectionsItemCard from '../components/CollectionsItems/CollectionsItemCard/CollectionsItemCard.vue'
 
 
 // to do :
 // 1. move useFetch into collectionItemsStore and use store herein
+//    - have a single store just for CollectionItemStore - since we need to use composable at top level,
+//      check pinia docs as you go..
 // 2. inject useData middleware btwn store and useFetch
 // 3. enhance useData and useFetch from prev/orig examples
+// 4. pagination - when/where do we want to break - have collections_item_list in simple (dedicated) store?
+
+const list = ref<CollectionsItemsList | null>(null)
+const is_loading = ref<boolean>(true)
 
 
+const collectionsItemsListStore = useCollectionsItemsListStore()
 
-const baseUrl = '/data/collection-items - sm.json'
+
+// const { payload, error, loading, fetchData } = useFetch<CollectionsItemsList>(baseUrl,{}) as UseFetchReturn<CollectionsItemsList>
+
+collectionsItemsListStore.load_collection_items()
 
 
-const { payload, error, loading, fetchData } = useFetch<CollectionsItemList>(baseUrl,{})
-
-onBeforeMount(async() => {
-   await fetchData()
+// we can't directly use payload - some TS confusion -  but we can use an intermediate 'list'
+watchEffect(() => {   
+   console.log('collectionsItemsListStore.loading.value',collectionsItemsListStore.loading)
+   is_loading.value = collectionsItemsListStore.loading.value
 })
-// const retry = () => {
-//   id.value = ''
-//   id.value = '1'
-// }
 
-// to do : 
-// - interject useData (endPoints)
-// - 
+watchEffect(() => {
+   list.value = <CollectionsItemsList>collectionsItemsListStore.collections_items_list
+   console.log('in here',collectionsItemsListStore.collections_items_list)
+})
 
-// to do : pagination - in CollectionsItemStore
 
 </script>
 
 <template>
-	<div v-if="error">
-    <p>Oops! Error encountered: {{ error }}</p>
-  </div>
-  <ul >
-      <li v-for="(item,index) in payload?.data" :key="index" >
-         <CollectionItemTeaser :item="item"/>
-      </li>
-  </ul>
-  <div v-if="loading">Loading...</div>
+      <!-- to do : error comes from useFetch - how to pass to client here? <div v-if="error">
+      <p>Oops! Error encountered: {{ error }}</p>
+   </div> -->
+   <div v-if="collectionsItemsListStore.loading">Loading...</div>
+   <section class="grid grid_cards_layout" style="margin-top:5rem;">
+      <!-- to do : the current big hit on 4000+ records is from loading imgs for all herein - this will be removed once we paginate and handle c20 at a time here... -->
+         <CollectionsItemCard v-for="(item,index) in list" :key="index"  :item="item as unknown as CollectionsItem" />
+   </section>
 </template>
 
 
