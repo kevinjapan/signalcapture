dbe0b9ff167e463f792d19e9ca1c9a7464a118cf
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-11-19 11:02:05 +0000

    add @ alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// npm install --save-dev @types/node required to find this module   to do : npm install and rollout '@' 
+import { fileURLToPath, URL } from 'url'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
