21d68962059e6b8e188a691a0732ec9df0b8d03e
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-11-20 14:22:34 +0000

    add is_valid_payload utility

---
+++ src/utilities/utilities/validation.ts
@@ -0,0 +1,10 @@
+
+
+
+export const is_valid_payload = (payload: object) => {
+    if(!payload) return false
+    const payload_required_keys = 'data,outcome,server_response'
+    let obj_keys = Object.keys(payload)
+    return obj_keys.sort().join(',') === payload_required_keys
+}
+
