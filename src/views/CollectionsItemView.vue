da589422bf00d63b2d11bd21806a9d076e2cc233
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-11-22 15:55:42 +0000

    display added fields

--- src/views/CollectionsItemView.vue
+++ src/views/CollectionsItemView.vue
@@ -23,6 +23,9 @@ watchEffect(() => {
    item.value = CollectionsItemStore.single_collection_item
 })
 
+
+// to do : ensure if a record doesn't have a field it isn't populated w/ last records field value
+
 </script>
 
 
@@ -36,17 +39,23 @@ watchEffect(() => {
       <div>file name</div><div>{{ item?.file_name }}</div>
       <div>file type</div><div>{{ item?.file_type }}</div>
       <div>folder</div><div>{{ item?.folder_path }}</div>
+      <div>keywords</div><div>{{ item?.keywords }}</div>
       <div>img desc</div><div>{{ item?.imgDesc }}</div>
       <div>item ref</div><div>{{ item?.item_ref }}</div>
       <div>item date</div><div>{{ item?.item_Date }}</div>
       <div>item type</div><div>{{ item?.item_Type }}</div>
+      <div>location</div><div>{{ item?.location }}</div>
       <div>author / creator</div><div>{{ item?.author_creator }}</div>
       <div>people</div><div>{{ item?.people }}</div>
       <div>source</div><div>{{ item?.source }}</div>
+      <div>donor</div><div>{{ item?.donor }}</div>
+      <div>owner</div><div>{{ item?.owner }}</div>
+      <div>copyright</div><div>{{ item?.copyright }}</div>
       <div>tags</div><div>{{ item?.tags }}</div>
       <div>id</div><div>{{ item?.id }}</div>
       <div>created</div><div>{{ item?.created_at }}</div>
       <div>last updated</div><div>{{ item?.updated_at }}</div>
+      <div>last viewed</div><div>{{ item?.viewed_at }}</div>
       <div>deleted</div><div>{{ item?.deleted_at }}</div>
    </section>
 
