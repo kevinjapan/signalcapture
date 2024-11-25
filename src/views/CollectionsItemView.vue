af0c058ec67e424d3b1814ccb9081ee92ac3e752
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-11-25 11:15:03 +0000

    use CollectionsItemRecord

--- src/views/CollectionsItemView.vue
+++ src/views/CollectionsItemView.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
 import { useCollectionsItemStore } from '../stores/CollectionsItemStore'
+import CollectionsItemRecord from '@/components/CollectionsItems/CollectionsItemRecord/CollectionsItemRecord.vue'
 
 
 
@@ -26,6 +27,9 @@ watchEffect(() => {
 
 // to do : ensure if a record doesn't have a field it isn't populated w/ last records field value
 
+// to do : on opening a record in search/browse, do we want to show some context - eg on left-side like in files?
+//          -  or just open in FilesView ? 
+
 </script>
 
 
@@ -33,31 +37,11 @@ watchEffect(() => {
 
    <div v-if="is_loading" class="loading_spin"></div>
 
-   <section v-else class="record_card">
-      <div>title</div><div>{{ item?.title }}</div>
-      <div>desc</div><div>{{ item?.content_desc }}</div>
-      <div>file name</div><div>{{ item?.file_name }}</div>
-      <div>file type</div><div>{{ item?.file_type }}</div>
-      <div>folder</div><div>{{ item?.folder_path }}</div>
-      <div>keywords</div><div>{{ item?.keywords }}</div>
-      <div>img desc</div><div>{{ item?.imgDesc }}</div>
-      <div>item ref</div><div>{{ item?.item_ref }}</div>
-      <div>item date</div><div>{{ item?.item_Date }}</div>
-      <div>item type</div><div>{{ item?.item_Type }}</div>
-      <div>location</div><div>{{ item?.location }}</div>
-      <div>author / creator</div><div>{{ item?.author_creator }}</div>
-      <div>people</div><div>{{ item?.people }}</div>
-      <div>source</div><div>{{ item?.source }}</div>
-      <div>donor</div><div>{{ item?.donor }}</div>
-      <div>owner</div><div>{{ item?.owner }}</div>
-      <div>copyright</div><div>{{ item?.copyright }}</div>
-      <div>tags</div><div>{{ item?.tags }}</div>
-      <div>id</div><div>{{ item?.id }}</div>
-      <div>created</div><div>{{ item?.created_at }}</div>
-      <div>last updated</div><div>{{ item?.updated_at }}</div>
-      <div>last viewed</div><div>{{ item?.viewed_at }}</div>
-      <div>deleted</div><div>{{ item?.deleted_at }}</div>
-   </section>
+    <CollectionsItemRecord
+        v-if="item"
+        :item="item"
+    />
+
 
 </template>
 
