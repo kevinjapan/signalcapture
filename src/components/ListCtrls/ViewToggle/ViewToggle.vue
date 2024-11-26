b6fea2e7f4eba4f91b090c185964ffd80bf98e77
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-11-26 12:08:57 +0000

    svgs from icons folder

--- src/components/ListCtrls/ViewToggle/ViewToggle.vue
+++ src/components/ListCtrls/ViewToggle/ViewToggle.vue
@@ -11,15 +11,17 @@ const emit = defineEmits([
 
 
 
+// to do : add 3rd option : grid-sm.svg
+
 </script>
 
 <template>
     <section class="m_.5">
         <button @click="$emit('toggle-view')">
             <img v-if="props.card_view"
-                src="../../../assets/imgs/grid.svg" />
+                src="../../../assets/icons/grid.svg" />
             <img v-else="!props.card_view"
-                src="../../../assets/imgs/list-columns-reverse.svg" />
+                src="../../../assets/icons/list-columns-reverse.svg" />
         </button>
     </section>
 </template>
