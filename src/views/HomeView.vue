f7e3d7e600545944af259a74535a7396b60a5d3e
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-12-03 11:27:56 +0000

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,20 +15,62 @@ watchEffect(() => {
 
 <template>
 
-   <section class="mt_5">
-      <h1>HomeView in here</h1>
+   <section class="dashboard mt_5">
+      
+      <section>
+         <h1>Dashboard</h1>
+      </section>
 
-      <p>Recent Searches</p>
-      <ul>
-         <li v-for="search_term in recent_searches_list">{{ search_term }}</li>
-      </ul>
+      <section class="site_status_notification">
+         <p>
+            Hello, this is a static-site demo and not all features of the full application are available.
+         </p>
+         <p>
+            This site is under development and not all design features are yet optimized for IOS platforms,
+            nor is the site yet responsive on mobile and smaller platforms.
+         </p>
+      </section>
+
+      <section>
+         <p>Recent Searches</p>
+         <ul>
+            <li class="recent_search" v-for="search_term in recent_searches_list">{{ search_term }}</li>
+         </ul>
+      </section>
+   
    </section>
 
 </template>
 
 
 <style scoped>
-ul {list-style:none;
-   user-select: none;}
-
+section.dashboard {
+   display:grid;
+   grid-template-columns:1fr;
+}
+section.dashboard section {
+   margin:1rem;
+   padding:1rem 2rem;
+   border:solid 1px white;
+   border-radius:.5rem;
+}
+@media (min-width: 768px) {
+   section.dashboard {
+      grid-template-columns:1fr 1fr 1fr;
+   }
+}
+ul {
+   list-style:none;
+   user-select: none;
+}
+section.site_status_notification {
+   border:solid 1px white;
+   border-radius:.5rem;
+}
+.recent_search {
+   width:fit-content;
+   padding:.15rem .5rem;
+   border-radius:.5rem;
+   cursor:pointer;
+}
 </style>
