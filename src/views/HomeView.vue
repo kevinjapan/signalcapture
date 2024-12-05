836602717cb5e39ea4d430c391b740e8e0a5b2ca
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-12-05 19:14:08 +0000

    add blueprint img

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,56 +1,69 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import RecentSearches from '@/components/Search/RecentSearches/RecentSearches.vue'
 
+onMounted(() => {
+	window.scroll(0,0)
+})
 </script>
 
 
 <template>
 
-   <section class="dashboard mt_5">
-      
-      <section>
-         <h1>Dashboard</h1>
-      </section>
+	<section class="dashboard mt_5">
+		
 
-      <section class="site_status_notification">
-         <p>
-            Hello, this is a static-site demo and not all features of the full application are available.
-         </p>
-         <p>
-            This site is under development and not all design features are yet optimized for IOS platforms,
-            nor is the site yet responsive on mobile and smaller platforms.
-         </p>
-      </section>
+		<section class="collections_blueprint">
 
-      <RecentSearches />
-   
-   </section>
+			<img src="/imgs/blueprint.jpg" />
+
+		</section>
+
+		<section class="site_status_notification">
+			<p>
+				Hello, this is a static-site demo and not all features of the full application are available.
+			</p>
+			<p>
+				This site is under development and not all design features are yet optimized for IOS platforms,
+				nor is the site yet responsive on mobile and smaller platforms.
+			</p>
+		</section>
+
+		<RecentSearches />
+	
+	</section>
 
 </template>
 
 
 <style scoped>
 section.dashboard {
-   display:grid;
-   grid-template-columns:1fr;
+	display:grid;
+	grid-template-columns:1fr;
 }
 section.dashboard section {
-   margin:1rem;
-   padding:1rem 2rem;
-   border:solid 1px white;
-   border-radius:.5rem;
+	margin:1rem;
+	padding:1rem 2rem;
+	/* border:solid 1px white; */
+	border-radius:.5rem;
 }
 @media (min-width: 768px) {
-   section.dashboard {
-      grid-template-columns:1fr 1fr 1fr;
-   }
+	section.dashboard {
+		grid-template-columns:1.5fr 1fr 1fr;
+	}
 }
 ul {
-   list-style:none;
-   user-select: none;
+	list-style:none;
+	user-select: none;
 }
 section.site_status_notification {
-   border:solid 1px white;
-   border-radius:.5rem;
+	/* border:solid 1px white; */
+	border-radius:.5rem;
+}
+
+img {
+	border:solid 1px black;
+	border-radius:.5rem;
+	max-width:100%;
 }
 </style>
